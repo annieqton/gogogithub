aebb3f73953e406434ac5a25cee4b39992beb5d4
Annie Ton-Nu <annieqton@gmail.com>
2017-04-05 16:02:55 -0700

    created xib file

---
+++ gogogithub/gogogithub/RepoNibCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepoNibCell.swift
+//  gogogithub
+//
+//  Created by Annie Ton-Nu on 4/5/17.
+//  Copyright © 2017 Annie Ton-Nu. All rights reserved.
+//
+
+import UIKit
+
+class RepoNibCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
