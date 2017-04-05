0a95b4f671636673556e4082ca3dfef6a654847e
Annie Ton-Nu <annieqton@gmail.com>
2017-04-05 16:34:40 -0700

    RepoNibCell.swift

--- gogogithub/gogogithub/RepoNibCell.swift
+++ gogogithub/gogogithub/RepoNibCell.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class RepoNibCell: UITableViewCell {
 
+    @IBOutlet weak var repoName: UILabel!
+    @IBOutlet weak var repoDescription: UILabel!
+    @IBOutlet weak var repoLanguage: UILabel!
+    @IBOutlet weak var repoStars: UILabel!
+    @IBOutlet weak var repoForked: UILabel!
+    @IBOutlet weak var repoCreated: UILabel!
+    
+    
+    var eachRepo: Repository! {
+        didSet {
+            self.repoName.text = eachRepo.name
+            self.repoDescription.text = eachRepo.description
+            self.repoLanguage.text = eachRepo.language
+            self.repoStars.text = "\(String(describing: eachRepo.stars))"
+            self.repoForked.text = "\(String(describing: eachRepo.forked))"
+            self.repoCreated.text = eachRepo.created
+            }
+        }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
--- gogogithub/gogogithub/Repository.swift
+++ gogogithub/gogogithub/Repository.swift
@@ -13,6 +13,9 @@ class Repository {
     let name: String
     let description: String?
     let language: String?
+    let stars: Int?
+    let forked: Bool?
+    let created: String?
     
     var repo: Repository?
     
@@ -22,12 +25,24 @@ class Repository {
         guard let name = json["name"] as? String else { return nil }
         self.name = name
         
-        if let description = json["description"] as? String, let language = json["language"] as? String {  //need to break this into 2 if let because if one returns nil, then it will fail
+        if let description = json["description"] as? String,
+            let language = json["language"] as? String,
+            let stars = json["targazers_count"] as? Int?,
+            let forked = json["forks_count"] as? Bool,
+            let created = json["created_at"] as? String?
+        {
             self.description = description
             self.language = language
+            self.stars = stars
+            self.forked = forked
+            self.created = created
+            
         } else {
             self.description = "No description available"
             self.language = "No specified language"
+            self.stars = 0
+            self.forked = false
+            self.created = "No date available"
         }
     }
 }
